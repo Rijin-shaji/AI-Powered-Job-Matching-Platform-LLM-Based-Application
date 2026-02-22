5fc1816edb270e7977f1b51e20b88a09f1ec048d
Rijin-shaji <rijinshajiedathil@gmail.com>
2026-02-22 09:03:00 +0530

    Add PDF loader to extract text from resumes

---
+++ src/pdf_loader.py
@@ -0,0 +1,9 @@
+import pdfplumber
+
+
+def load_resume(pdf_path):
+    text = ""
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            text += page.extract_text() + "\n"
+    return text
